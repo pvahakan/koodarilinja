a6518c0b57adce67afab91253370bf2cb80f8c2a
pvahakan <pauli.vahakangas@gmail.com>
2022-11-18 12:31:06 +0200

    Muokattu tarkistusfunktiot ottamaan sanalista argumenttina

--- kirjoitusnopeustesti/main.py
+++ kirjoitusnopeustesti/main.py
@@ -4,34 +4,35 @@ import os
 sanat = ['hei', 'moi', 'kyllä', 'ei', 'tervetuloa']
 kirjoitetut_sanat = []
 
-def tarkista_oikeat_sanat(kirjoitetut_sanat):
+def tarkista_oikeat_sanat(sanalista, kirjoitetut_sanat):
     oikein = 0
     for sana in kirjoitetut_sanat:
-        if sana in sanat:
+        if sana in sanalista:
             oikein += 1
     return oikein
 
-def tarkista_vaarat_sanat(kirjoitetut_sanat):
-    oikein = tarkista_oikeat_sanat(kirjoitetut_sanat)
+def tarkista_vaarat_sanat(sanalista, kirjoitetut_sanat):
+    oikein = tarkista_oikeat_sanat(sanalista, kirjoitetut_sanat)
     return len(kirjoitetut_sanat) - oikein
 
-def tulosta_raportti(kirjoitetut_sanat):
-    oikein = tarkista_oikeat_sanat(kirjoitetut_sanat)
-    vaarin = tarkista_vaarat_sanat(kirjoitetut_sanat)
+def tulosta_raportti(sanalista, kirjoitetut_sanat):
+    oikein = tarkista_oikeat_sanat(sanalista, kirjoitetut_sanat)
+    vaarin = tarkista_vaarat_sanat(sanalista, kirjoitetut_sanat)
     print(f'Kirjoitit oikein {oikein}/{len(kirjoitetut_sanat)} sanaa')
     print(f'Kirjoitit väärin {vaarin}/{len(kirjoitetut_sanat)} sanaa')
 
-i = 0
-while i < 5:
+
+sanojen_maara = 5
+
+while True:
     os.system('cls') # Windows
     # os.system('clear') # Linux
     print(sanat[random.randint(0, len(sanat)-1)])
     sana = input('>> ')
     kirjoitetut_sanat.append(sana)
 
-    if sana == 'q':
+    if len(kirjoitetut_sanat) >= sanojen_maara:
         break
 
-    i += 1
 
-tulosta_raportti(kirjoitetut_sanat)
+tulosta_raportti(sanat, kirjoitetut_sanat)

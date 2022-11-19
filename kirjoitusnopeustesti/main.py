3a85ab8a2264a1a53e626e0428432ed7aaedcb92
pvahakan <pauli.vahakangas@gmail.com>
2022-11-19 17:46:08 +0200

    Perusversion toiminnallisuus toteutettu

--- kirjoitusnopeustesti/main.py
+++ kirjoitusnopeustesti/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 import os
 
 sanat = ['hei', 'moi', 'kyllä', 'ei', 'tervetuloa']
@@ -15,24 +16,51 @@ def tarkista_vaarat_sanat(sanalista, kirjoitetut_sanat):
     oikein = tarkista_oikeat_sanat(sanalista, kirjoitetut_sanat)
     return len(kirjoitetut_sanat) - oikein
 
-def tulosta_raportti(sanalista, kirjoitetut_sanat):
+def tulosta_raportti(sanalista, kirjoitetut_sanat, kesto):
     oikein = tarkista_oikeat_sanat(sanalista, kirjoitetut_sanat)
     vaarin = tarkista_vaarat_sanat(sanalista, kirjoitetut_sanat)
+    cpm = laske_merkit_minuutissa(kirjoitetut_sanat, kesto)
+    wpm = laske_sanat_minuutissa(kirjoitetut_sanat, kesto)
+    os.system('clear')
+    print(f'Kirjoittamiseen kului aikaa {round(kesto)} sekuntia\n')
     print(f'Kirjoitit oikein {oikein}/{len(kirjoitetut_sanat)} sanaa')
-    print(f'Kirjoitit väärin {vaarin}/{len(kirjoitetut_sanat)} sanaa')
+    print(f'Kirjoitit väärin {vaarin}/{len(kirjoitetut_sanat)} sanaa\n')
+    print(f'Kirjoitusnopeutesi on {round(cpm)} merkkiä/min eli {round(wpm)} sanaa/min\n')
+    input('Paina mitä tahansa lopettaaksesi')
+
+def laske_merkit_minuutissa(kirjoitetut_sanat, kesto):
+    kesto = kesto / 60 # Muutetaan minuuteiksi
+    merkkien_maara = 0
+    for sana in kirjoitetut_sanat:
+        merkkien_maara += len(sana)
+
+    return merkkien_maara / kesto
+
+def laske_sanat_minuutissa(kirjoitetut_sanat, kesto):
+    kesto = kesto / 60 # Muutetaan minuuteiksi
+    merkkien_maara = 0
+    for sana in kirjoitetut_sanat:
+        merkkien_maara += len(sana)
+    
+    sanoja = round(merkkien_maara / 5)
+    return sanoja / kesto
+
 
 
 sanojen_maara = 5
+aloitusaika = time.time()
 
 while True:
-    os.system('cls') # Windows
-    # os.system('clear') # Linux
+    # os.system('cls') # Windows
+    os.system('clear') # Linux
     print(sanat[random.randint(0, len(sanat)-1)])
     sana = input('>> ')
     kirjoitetut_sanat.append(sana)
 
     if len(kirjoitetut_sanat) >= sanojen_maara:
+        lopetusaika = time.time()
         break
 
 
-tulosta_raportti(sanat, kirjoitetut_sanat)
+kesto = lopetusaika - aloitusaika
+tulosta_raportti(sanat, kirjoitetut_sanat, kesto)

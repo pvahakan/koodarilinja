e491575308e1f9daec0064f6b4b91c41256fb17a
pvahakan <pauli.vahakangas@gmail.com>
2023-02-03 14:18:56 +0200

    Siistitty pääohjelmasta turhat kommentit pois

--- matopeli/main.py
+++ matopeli/main.py
@@ -1,60 +1,11 @@
 #!/usr/bin/env python3
 
-
-# Pseudokoodi siitä, miten peli tulisi rakentaa. Luodaan toteutus
-# luokkien avulla tämän pohjalta.
-
-# # Luodaan objektit
-# solun_koko = 20
-# solujen_maara = 20
-# 
-# kentta = Kentta()
-# ruoka = Ruoka()
-# snake = Snake()
-# logiikka = Logiikka()
-# 
-# while running:
-#     # Tarkistetaan käyttäjän syötteet
-#     syote = Kayttoliittyma.hae_syotteet()
-#     if syote == lopeta:
-#         running = False
-#         Kayttoliittyma.lopeta()
-#     if syote == vasemmalle:
-#         snake.liikuta_vasemmalle()
-#     if syote == oikealle:
-#         snake.liikuta_oikealle()
-#     if syote == ylos:
-#         snake.liikuta_ylos()
-#     if syote == alas:
-#         snake.liikuta_alas()
-# 
-#     # Päivitetään snaken sijainti
-#     snake.paivita()
-# 
-#     # Tarkistetaan osuma seiniin, itseen tai ruokaan
-#     osuma = logiikka.tarkista_osuma()
-#     if osuma == None:
-#         pass
-#     elif osuma == reuna:
-#         kayttoliittyma.lopeta()
-#     elif osuma == snake:
-#         kayttoliittyma.lopeta()
-#     elif osuma == ruoka:
-#         snake.kasvata()
-#         snake.lisaa_piste()
-#         ruoka.uusi_sijainti()
-#     
-#     # Piirretään elementit
-#     kayttoliittyma.piirra_kentta()
-#     kayttoliittyma.piirra_snake()
-#     kayttoliittyma.piirra_ruoka()
+import time
 
 from grid_game import Peli
 from grid_game import Snake
 from grid_game import Nappaimisto
 from grid_game import Ruoka
-
-import time
 
 peli = Peli(25) # Luodaan pelikenttä, 25x25 -ruutua. Ruudun koko määräytyy Peli-luokassa.
 mato = Snake(peli)
@@ -62,10 +13,6 @@ ruoka = Ruoka(peli)
 nappaimisto = Nappaimisto()
 
 syominen = False
-
-# Hoidetaan nämä luokkien mato ja ruoka konstruktoreissa
-# peli.lisaa_mato(mato) # Lisätään mato pelikentälle
-# peli.lisaa_ruoka(ruoka) # Lisätään ruoka
 
 if __name__ == '__main__':
     while peli.running:
